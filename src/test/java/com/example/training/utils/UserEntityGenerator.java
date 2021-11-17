dfaf63902f850b30cda04f432c73031243781a1b
xalivan <xalivan@ukr.net>
2021-11-17 11:22:24 +0200

    4.7 UNit test of User Service

---
+++ src/test/java/com/example/training/utils/UserEntityGenerator.java
@@ -0,0 +1,26 @@
+package com.example.training.utils;
+
+import com.example.training.model.Role;
+import com.example.training.model.UserEntity;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Random;
+import java.util.UUID;
+
+import static java.util.UUID.randomUUID;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserEntityGenerator {
+     final static Random RANDOM = new Random();
+
+    private static Role getRole(){
+        Role[] roles = Role.values();
+        return roles[RANDOM.nextInt(roles.length)];
+    }
+
+    public static UserEntity generateUser() {
+        return new UserEntity(RANDOM.nextInt(1000), UUID.randomUUID().toString(), UUID.randomUUID().toString(), randomUUID().toString(), getRole());
+    }
+
+}
