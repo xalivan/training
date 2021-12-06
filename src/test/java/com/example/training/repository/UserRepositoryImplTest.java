4e6d9f14903a836de364644059a6f7eff8a20b71
xalivan <xalivan@ukr.net>
2021-12-06 16:06:40 +0200

    11.3 Update Repository Test

--- src/test/java/com/example/training/repository/UserRepositoryImplTest.java
+++ src/test/java/com/example/training/repository/UserRepositoryImplTest.java
@@ -1,14 +1,13 @@
 package com.example.training.repository;
 
 import com.example.training.model.UserEntity;
-import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static com.example.training.jooq.tables.Role.ROLE;
 import static com.example.training.jooq.tables.UserEntity.USER_ENTITY;
@@ -18,7 +17,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.core.Is.is;
 
-@RequiredArgsConstructor
 class UserRepositoryImplTest {
     private final DSLContext dsl = getConnection();
     private final UserRepository repository = new UserRepositoryImpl(dsl);
@@ -32,50 +30,29 @@ class UserRepositoryImplTest {
     void insert() {
         UserEntity user = generateUser();
 
-        int insert = repository.insert(user);
-        Optional<UserEntity> userEntity = dsl.select(USER_ENTITY.ID, USER_ENTITY.FIRST_NAME, USER_ENTITY.LAST_NAME, USER_ENTITY.PASSWORD, ROLE.ROLE_)
-                .from(USER_ENTITY)
-                .join(ROLE).on(USER_ENTITY.ROLE.eq(ROLE.ROLE_ID))
-                .where(USER_ENTITY.ID.eq(user.getId()))
-                .fetchOptionalInto(UserEntity.class);
-        UserEntity[] actual = userEntity.stream().toArray(UserEntity[]::new);
-
-        assertThat(insert, is(1));
-        assertThat(Arrays.asList(actual), containsInAnyOrder(user));
+        assertThat(repository.insert(user), is(1));
+        assertThat(getUserEntityById(user.getId()), is(Optional.of(user)));
     }
 
     @Test
     void update() {
         UserEntity user = generateUser();
-        dsl.insertInto(USER_ENTITY, USER_ENTITY.ID, USER_ENTITY.FIRST_NAME, USER_ENTITY.LAST_NAME, USER_ENTITY.PASSWORD, USER_ENTITY.ROLE)
-                .values(user.getId(), user.getFirstName(), user.getLastName(), user.getPassword(), user.getRole().getId())
-                .execute();
-        Optional<UserEntity> userEntity = dsl.select(USER_ENTITY.ID, USER_ENTITY.FIRST_NAME, USER_ENTITY.LAST_NAME, USER_ENTITY.PASSWORD, ROLE.ROLE_)
-                .from(USER_ENTITY)
-                .join(ROLE).on(USER_ENTITY.ROLE.eq(ROLE.ROLE_ID))
-                .where(USER_ENTITY.ID.eq(user.getId()))
-                .fetchOptionalInto(UserEntity.class);
-        UserEntity[] actual = userEntity.stream().toArray(UserEntity[]::new);
-        int update = repository.update(user);
+        insertUserEntity(user);
+        Optional<UserEntity> userEntity = getUserEntityById(user.getId());
+        List<UserEntity> actual = userEntity.stream().collect(Collectors.toList());
 
-        assertThat(Arrays.asList(actual), containsInAnyOrder(user));
-        assertThat(update, is(1));
+        assertThat(actual, containsInAnyOrder(user));
+        assertThat(repository.update(user), is(1));
     }
 
     @Test
     void delete() {
         UserEntity user = generateUser();
-        dsl.insertInto(USER_ENTITY, USER_ENTITY.ID, USER_ENTITY.FIRST_NAME, USER_ENTITY.LAST_NAME, USER_ENTITY.PASSWORD, USER_ENTITY.ROLE)
-                .values(user.getId(), user.getFirstName(), user.getLastName(), user.getPassword(), user.getRole().getId())
-                .execute();
-        int delete = repository.delete(user.getId());
-        Optional<UserEntity> userEntity = dsl.select(USER_ENTITY.ID, USER_ENTITY.FIRST_NAME, USER_ENTITY.LAST_NAME, USER_ENTITY.PASSWORD, ROLE.ROLE_)
-                .from(USER_ENTITY)
-                .join(ROLE).on(USER_ENTITY.ROLE.eq(ROLE.ROLE_ID))
-                .where(USER_ENTITY.ID.eq(user.getId()))
-                .fetchOptionalInto(UserEntity.class);
+        insertUserEntity(user);
 
-        assertThat(delete, is(1));
+        assertThat(repository.delete(user.getId()), is(1));
+
+        Optional<UserEntity> userEntity = getUserEntityById(user.getId());
         assertThat(userEntity.isEmpty(), is(true));
     }
 
@@ -83,39 +60,43 @@ class UserRepositoryImplTest {
     void findAll() {
         UserEntity user1 = generateUser();
         UserEntity user2 = generateUser();
-        dsl.insertInto(USER_ENTITY, USER_ENTITY.ID, USER_ENTITY.FIRST_NAME, USER_ENTITY.LAST_NAME, USER_ENTITY.PASSWORD, USER_ENTITY.ROLE)
-                .values(user1.getId(), user1.getFirstName(), user1.getLastName(), user1.getPassword(), user1.getRole().getId())
-                .execute();
-        dsl.insertInto(USER_ENTITY, USER_ENTITY.ID, USER_ENTITY.FIRST_NAME, USER_ENTITY.LAST_NAME, USER_ENTITY.PASSWORD, USER_ENTITY.ROLE)
-                .values(user2.getId(), user2.getFirstName(), user2.getLastName(), user2.getPassword(), user2.getRole().getId())
-                .execute();
-        List<UserEntity> list = repository.findAll();
-        UserEntity[] actual = list.toArray(UserEntity[]::new);
+        insertUserEntity(user1);
+        insertUserEntity(user2);
 
-        assertThat(Arrays.asList(actual), containsInAnyOrder(user1, user2));
+        assertThat(repository.findAll(), containsInAnyOrder(user1, user2));
     }
 
     @Test
     void findById() {
         UserEntity user = generateUser();
-        dsl.insertInto(USER_ENTITY, USER_ENTITY.ID, USER_ENTITY.FIRST_NAME, USER_ENTITY.LAST_NAME, USER_ENTITY.PASSWORD, USER_ENTITY.ROLE)
-                .values(user.getId(), user.getFirstName(), user.getLastName(), user.getPassword(), user.getRole().getId())
-                .execute();
+        insertUserEntity(user);
         Optional<UserEntity> userEntity = repository.findById(user.getId());
-        UserEntity[] actual = userEntity.stream().toArray(UserEntity[]::new);
+        List<UserEntity> actual = userEntity.stream().collect(Collectors.toList());
 
-        assertThat(Arrays.asList(actual), containsInAnyOrder(user));
+        assertThat(actual, containsInAnyOrder(user));
     }
 
     @Test
     void findOneByLastName() {
         UserEntity user = generateUser();
+        insertUserEntity(user);
+        Optional<UserEntity> userEntity = repository.findOneByLastName(user.getLastName());
+        List<UserEntity> actual = userEntity.stream().collect(Collectors.toList());
+
+        assertThat(actual, containsInAnyOrder(user));
+    }
+
+    private Optional<UserEntity> getUserEntityById(int id) {
+        return dsl.select(USER_ENTITY.ID, USER_ENTITY.FIRST_NAME, USER_ENTITY.LAST_NAME, USER_ENTITY.PASSWORD, ROLE.ROLE_)
+                .from(USER_ENTITY)
+                .join(ROLE).on(USER_ENTITY.ROLE.eq(ROLE.ROLE_ID))
+                .where(USER_ENTITY.ID.eq(id))
+                .fetchOptionalInto(UserEntity.class);
+    }
+    private void insertUserEntity(UserEntity user){
         dsl.insertInto(USER_ENTITY, USER_ENTITY.ID, USER_ENTITY.FIRST_NAME, USER_ENTITY.LAST_NAME, USER_ENTITY.PASSWORD, USER_ENTITY.ROLE)
                 .values(user.getId(), user.getFirstName(), user.getLastName(), user.getPassword(), user.getRole().getId())
                 .execute();
-        Optional<UserEntity> userEntity = repository.findOneByLastName(user.getLastName());
-        UserEntity[] actual = userEntity.stream().toArray(UserEntity[]::new);
-
-        assertThat(Arrays.asList(actual), containsInAnyOrder(user));
     }
+
 }
--- src/test/java/com/example/training/utils/TestDatabaseConnectionManager.java
+++ src/test/java/com/example/training/utils/TestDatabaseConnectionManager.java
@@ -7,9 +7,9 @@ import org.jooq.impl.DSL;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TestDatabaseConnectionManager {
-    static String DB_URL = "jdbc:postgresql://localhost:5435/test";
-    static String DB_USER = "admin";
-    static String DB_PASSWORD = "1111";
+    private static String DB_URL = "jdbc:postgresql://localhost:5435/test";
+    private static String DB_USER = "admin";
+    private static String DB_PASSWORD = "1111";
 
     public static DSLContext getConnection() {
         return DSL.using(DB_URL, DB_USER, DB_PASSWORD);
